50d27fd067152815428d96435c660c906426133b
Uğur Biçer <uuur86@yandex.com>
2022-10-20 06:11:31 +0300

    Updated build dist files

--- dist/index.modern.js
+++ dist/index.modern.js
@@ -4,36 +4,78 @@ import { TabList, Tab, TabPanel, Tabs } from 'react-tabs';
 import { Button } from 'primereact/button';
 import { UniqueComponentId } from 'primereact/utils';
 
-function ModalBase({
-  buttonLabel = 'Open Modal',
-  buttonConfigs = {},
-  status = false,
-  header = false,
-  footer = false,
-  onClose = () => {},
-  children,
-  ...props
-}) {
-  const id = UniqueComponentId();
-  const [visible, setVisible] = useState(false);
-  const triggerButton = status => {
+function _extends() {
+  _extends = Object.assign ? Object.assign.bind() : function (target) {
+    for (var i = 1; i < arguments.length; i++) {
+      var source = arguments[i];
+      for (var key in source) {
+        if (Object.prototype.hasOwnProperty.call(source, key)) {
+          target[key] = source[key];
+        }
+      }
+    }
+    return target;
+  };
+  return _extends.apply(this, arguments);
+}
+function _objectDestructuringEmpty(obj) {
+  if (obj == null) throw new TypeError("Cannot destructure " + obj);
+}
+function _objectWithoutPropertiesLoose(source, excluded) {
+  if (source == null) return {};
+  var target = {};
+  var sourceKeys = Object.keys(source);
+  var key, i;
+  for (i = 0; i < sourceKeys.length; i++) {
+    key = sourceKeys[i];
+    if (excluded.indexOf(key) >= 0) continue;
+    target[key] = source[key];
+  }
+  return target;
+}
+
+var _excluded = ["buttonLabel", "buttonConfigs", "status", "header", "footer", "onClose", "children"],
+  _excluded2 = ["children"],
+  _excluded3 = ["status", "tabIndex", "footer", "children"];
+function ModalBase(_ref) {
+  var _ref$buttonLabel = _ref.buttonLabel,
+    buttonLabel = _ref$buttonLabel === void 0 ? 'Open Modal' : _ref$buttonLabel,
+    _ref$buttonConfigs = _ref.buttonConfigs,
+    buttonConfigs = _ref$buttonConfigs === void 0 ? {} : _ref$buttonConfigs,
+    _ref$status = _ref.status,
+    status = _ref$status === void 0 ? false : _ref$status,
+    _ref$header = _ref.header,
+    header = _ref$header === void 0 ? false : _ref$header,
+    _ref$footer = _ref.footer,
+    footer = _ref$footer === void 0 ? false : _ref$footer,
+    _ref$onClose = _ref.onClose,
+    onClose = _ref$onClose === void 0 ? function () {} : _ref$onClose,
+    children = _ref.children,
+    props = _objectWithoutPropertiesLoose(_ref, _excluded);
+  var id = UniqueComponentId();
+  var _useState = useState(false),
+    visible = _useState[0],
+    setVisible = _useState[1];
+  var triggerButton = function triggerButton(status) {
     return React.createElement(ActionButton, {
-      onClick: () => !status && setVisible(true),
+      onClick: function onClick() {
+        return !status && setVisible(true);
+      },
       disabled: status,
       label: buttonLabel
     });
   };
-  const onHide = () => {
+  var onHide = function onHide() {
     onClose();
     setVisible(false);
   };
-  useEffect(() => {
+  useEffect(function () {
     console.log('status', status);
     setVisible(status);
   }, [status]);
-  const dialog = visible => {
-    const footerProps = (buttonConfigs === null || buttonConfigs === void 0 ? void 0 : buttonConfigs.footer) && ButtonsTemplate(buttonConfigs.footer) || footer;
-    const HeaderProps = (buttonConfigs === null || buttonConfigs === void 0 ? void 0 : buttonConfigs.header) && ButtonsTemplate(buttonConfigs.header) || header;
+  var dialog = function dialog(visible) {
+    var footerProps = (buttonConfigs === null || buttonConfigs === void 0 ? void 0 : buttonConfigs.footer) && ButtonsTemplate(buttonConfigs.footer) || footer;
+    var HeaderProps = (buttonConfigs === null || buttonConfigs === void 0 ? void 0 : buttonConfigs.header) && ButtonsTemplate(buttonConfigs.header) || header;
     return React.createElement(Dialog, Object.assign({
       contentStyle: {
         width: 'auto'
@@ -51,10 +93,14 @@ function ModalBase({
     if (!Array.isArray(configs) || configs.length < 1) {
       return null;
     }
-    const buttons = configs.map((button, index) => {
-      button = Object.fromEntries(Object.entries(button).filter(([key, value]) => key && value));
+    var buttons = configs.map(function (button, index) {
+      button = Object.fromEntries(Object.entries(button).filter(function (_ref2) {
+        var key = _ref2[0],
+          value = _ref2[1];
+        return key && value;
+      }));
       return React.createElement(ActionButton, Object.assign({}, button, {
-        onClick: () => {
+        onClick: function onClick() {
           setVisible(false);
           button.onClick();
         },
@@ -68,55 +114,61 @@ function ModalBase({
   }
   return React.createElement("div", null, triggerButton(visible), dialog(visible));
 }
-function Modal({
-  children,
-  ...props
-}) {
+function Modal(_ref3) {
+  var children = _ref3.children,
+    props = _objectWithoutPropertiesLoose(_ref3, _excluded2);
   return React.createElement("div", null, React.createElement(ModalBase, Object.assign({}, props), children));
 }
-function ActionButton({
-  ...props
-}) {
-  const promptId = UniqueComponentId(`propmt-button-${(props === null || props === void 0 ? void 0 : props.name) || ''}`);
+function ActionButton(_ref4) {
+  var props = _extends({}, (_objectDestructuringEmpty(_ref4), _ref4));
+  var promptId = UniqueComponentId("propmt-button-" + ((props === null || props === void 0 ? void 0 : props.name) || ''));
   return React.createElement(Button, Object.assign({}, props, {
     id: promptId,
     name: props === null || props === void 0 ? void 0 : props.name
   }));
 }
-function PromptModal({
-  buttonLabel = 'Open',
-  labelYes = 'Yes',
-  labelNo = 'No',
-  onYes,
-  onNo,
-  children
-}) {
-  const handler = answer => answer === 'yes' ? onYes && onYes() : onNo && onNo();
-  const modalBase = React.createElement(ModalBase, {
+function PromptModal(_ref5) {
+  var _ref5$buttonLabel = _ref5.buttonLabel,
+    buttonLabel = _ref5$buttonLabel === void 0 ? 'Open' : _ref5$buttonLabel,
+    _ref5$labelYes = _ref5.labelYes,
+    labelYes = _ref5$labelYes === void 0 ? 'Yes' : _ref5$labelYes,
+    _ref5$labelNo = _ref5.labelNo,
+    labelNo = _ref5$labelNo === void 0 ? 'No' : _ref5$labelNo,
+    onYes = _ref5.onYes,
+    onNo = _ref5.onNo,
+    children = _ref5.children;
+  var handler = function handler(answer) {
+    return answer === 'yes' ? onYes && onYes() : onNo && onNo();
+  };
+  var modalBase = React.createElement(ModalBase, {
     status: false,
     buttonLabel: buttonLabel,
     buttonConfigs: {
       footer: [{
         label: labelYes,
         className: 'bg-green-100 text-green-700 align-right',
-        onClick: () => handler('yes'),
+        onClick: function onClick() {
+          return handler('yes');
+        },
         hide: false
       }, {
         label: labelNo,
         className: 'bg-pink-100 text-pink-700 align-left',
-        onClick: () => handler('no'),
+        onClick: function onClick() {
+          return handler('no');
+        },
         hide: false
       }]
     }
   }, children);
   return React.createElement("div", null, modalBase);
 }
-function TabModalWrapper({
-  steps = [],
-  children
-}) {
-  const tabList = React.createElement("div", null, React.createElement(TabList, null, steps.map((step, index) => {
-    const fieldDisabled = !!step.disabled;
+function TabModalWrapper(_ref6) {
+  var _ref6$steps = _ref6.steps,
+    steps = _ref6$steps === void 0 ? [] : _ref6$steps,
+    children = _ref6.children;
+  var tabList = React.createElement("div", null, React.createElement(TabList, null, steps.map(function (step, index) {
+    var fieldDisabled = !!step.disabled;
     return React.createElement(Tab, {
       key: index,
       disabled: fieldDisabled,
@@ -126,45 +178,50 @@ function TabModalWrapper({
       } : {}
     }, step.label);
   })));
-  const tabPanels = steps.map((step, index) => React.createElement(TabPanel, {
-    key: index
-  }, step.content(step.params || null)));
+  var tabPanels = steps.map(function (step, index) {
+    return React.createElement(TabPanel, {
+      key: index
+    }, step.content(step.params || null));
+  });
   return children({
-    tabList,
-    tabPanels,
+    tabList: tabList,
+    tabPanels: tabPanels,
     stepData: steps
   });
 }
-function TabModal({
-  status = false,
-  tabIndex: tabInd = 0,
-  footer,
-  children,
-  ...props
-}) {
-  const [tabIndex, setTabIndex] = useState(tabInd);
+function TabModal(_ref7) {
+  var _ref7$status = _ref7.status,
+    status = _ref7$status === void 0 ? false : _ref7$status,
+    _ref7$tabIndex = _ref7.tabIndex,
+    tabInd = _ref7$tabIndex === void 0 ? 0 : _ref7$tabIndex,
+    children = _ref7.children,
+    props = _objectWithoutPropertiesLoose(_ref7, _excluded3);
+  var _useState2 = useState(tabInd),
+    tabIndex = _useState2[0],
+    setTabIndex = _useState2[1];
   return React.createElement("div", null, React.createElement(ModalBase, {
     status: status
-  }, React.createElement(TabModalWrapper, Object.assign({}, props), ({
-    tabList,
-    tabPanels
-  }) => {
+  }, React.createElement(TabModalWrapper, Object.assign({}, props), function (_ref8) {
+    var tabList = _ref8.tabList,
+      tabPanels = _ref8.tabPanels;
     return React.createElement("div", {
       className: 'mt-5 flex'
     }, React.createElement(Tabs, {
       selectedIndex: tabIndex,
-      onSelect: index => setTabIndex(index)
+      onSelect: function onSelect(index) {
+        return setTabIndex(index);
+      }
     }, children({
-      tabList,
-      tabPanels
+      tabList: tabList,
+      tabPanels: tabPanels
     })));
   })));
 }
 var index = {
-  Modal,
-  TabModal,
-  PromptModal,
-  ActionButton
+  Modal: Modal,
+  TabModal: TabModal,
+  PromptModal: PromptModal,
+  ActionButton: ActionButton
 };
 
 export default index;
